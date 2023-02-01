12c6098d7f1f0de9113e1954107ca53ca5ddc920
DOG-BREAD <91770692+DOG-BREAD@users.noreply.github.com>
2023-02-01 14:09:53 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+//ReactDom.render(What to show , where to show , optinoal callback)
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
